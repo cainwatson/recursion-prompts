c4f490f5f14519bed1c4964ba2c44400d13bb8e2
Operation Spark <opspark@Operations-MacBook-Air.local>
2017-06-19 15:20:58 -0500

    sum done

--- src/recursion.js
+++ src/recursion.js
@@ -16,6 +16,13 @@ var factorial = function(n) {
 // 2. Compute the sum of an array of integers.
 // Example:  sum([1, 2, 3, 4, 5, 6]);  // 21
 var sum = function(array) {
+    let arr = array.slice();
+    if (arr.length === 0) {
+        return 0
+    } else if (arr.length === 1) {
+        return array[0]
+    }
+    return arr.shift() + sum(arr);
 };
 
 // 3. Sum all numbers in an array containing nested arrays.
