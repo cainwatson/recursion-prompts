321e1dbd0a21e9645b024f23f53e5d14855d824b
Operation Spark <opspark@Operations-MacBook-Air.local>
2017-06-21 11:13:47 -0500

    completed reverse

--- src/recursion.js
+++ src/recursion.js
@@ -64,6 +64,14 @@ var range = function(x, y) {
 // Example:  exponent(4,3);  // 64
 // https://www.khanacademy.org/computing/computer-science/algorithms/recursive-algorithms/a/computing-powers-of-a-number
 var exponent = function(base, exp) {
+    if (exp === 0) {
+        return 1;
+    } else if (exp === 1 || exp === -1) {
+        return base;
+    } else if (exp < 0) {
+        return 1/(base * exponent(base, exp+1));
+    }
+    return base * exponent(base, exp-1);
 };
 
 // 8. Determine if a number is a power of two.
@@ -75,6 +83,10 @@ var powerOfTwo = function(n) {
 
 // 9. Write a function that accepts a string a reverses it.
 var reverse = function(string) {
+    if (string.length === 1) {
+        return string;
+    }
+    return string.slice(string.length-1) + reverse(string.slice(0, string.length-1))
 };
 
 // 10. Write a function that determines if a string is a palindrome.
